0e0037e13479ccccb23fcb13de033a3a2c9828cb
Gagansharma09 <163728342+Gagansharma09@users.noreply.github.com>
2026-06-05 06:19:57 +0530

    Comment out counter logic implementation
    Commented out the always_ff block for counter logic.

--- 01-counter/src/counter.sv
+++ 01-counter/src/counter.sv
@@ -18,8 +18,23 @@ module updown_counter(
     input logic [3:0] d_in,// Input data for loading
     output logic [3:0] count // Counter output
 );
-
-
+  
+    /*solution 
+    always_ff @(posedge clk or negedge rst_n) begin
+        if(!rst_n)
+            count <= 4'b0000;
+        else if (load)
+            count <= d_in;
+        else if(enable)begin
+                if(up_down)
+                        count <= count + 1'b1;
+                else
+                        count <= count - 1'b1;
+        end
+        else 
+                        count <= count;
+        end
+    /*
 endmodule
 
 
